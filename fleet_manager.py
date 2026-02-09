c062c88dade0f93f5e8f4b8fd1148f86e8647e64
JS2437 <js2437@kent.ac.uk>
2026-02-09 19:23:33 +0000

    Created display_menu and added to main

--- fleet_manager.py
+++ fleet_manager.py
@@ -2,7 +2,9 @@ def main():
     names, ranks, divisions, ids = init_database()
     uName = input("Please enter your full name: ")
 
-    print(uName, " logged in!")
+    print(uName, " succesfully logged in.")
+    while True:
+        opt = display_menu()
 
 
 
@@ -14,5 +16,17 @@ def init_database():
     r = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Cadet"]
     d = ["Command", "Command", "Operations", "Security", "Sciences"]
     i = ["1", "2", "3", "4", "5"]
-
     return n, r, d, i
+
+def display_menu():
+    print("\n--- MENU ---")
+    print("1) View Crew Members")
+    print("2) Add Crew Member")
+    print("3) Remove Crew Member")
+    print("4) Update Rank")
+    print("5) Search Crew Name")
+    print("6) Filter By Division")
+    print("7) Calculate Payroll")
+    print("8) Count Officers")
+    opt = input("Select option: ")
+    return opt
