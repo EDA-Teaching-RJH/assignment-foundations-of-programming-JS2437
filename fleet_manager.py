d98c2d6e62fb60ed9b8ae6658b1991bdede745de
JS2437 <js2437@kent.ac.uk>
2026-02-09 19:15:59 +0000

    Added init_database

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,2 +1,7 @@
 def init_database():
-    n = ["Picard"]
+    n = ["Picard", "Riker", "Data", "Worf", "Zeph"]
+    r = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Cadet"]
+    d = ["Command", "Command", "Operations", "Security", "Sciences"]
+    i = ["1", "2", "3", "4", "5"]
+
+    return n, r, d, i
