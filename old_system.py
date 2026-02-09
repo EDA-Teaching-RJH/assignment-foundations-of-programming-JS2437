19609256aca6d428e756dd818f1e8c50dcf56e05
JS2437 <js2437@kent.ac.uk>
2026-02-09 18:13:37 +0000

    Accounts for invalid input to remove

--- old_system.py
+++ old_system.py
@@ -46,12 +46,20 @@ def run_system_monolith():
             
         elif opt == "3":
             rem = input("Name to remove: ")
-           
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
+            Validname = False
+            for i in range(len(n)):
+                if rem == n(i):
+                    idx = n.index(rem)
+                    Validname = True
+
+            if Validname == True:
+                    n.pop(idx)
+                    r.pop(idx)
+                    d.pop(idx)
+                    print("Removed")
+            else:
+                print("Invalid")
+                
             
         elif opt == "4":
             print("Analyzing...")
